648807026c45bdd4b74647cbf8e6f72b41cf47de
Ana Carolina Silva Cerqueira <anikcerqueira@hotmail.com>
2024-11-10 19:27:52 +0100

    firts commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }  else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu'); 
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+  }  else {
+    menuMobile.classList.add('open');
+  }
+}
